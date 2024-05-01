e248e65101f42f9effd9fd6313204efa84e8afc8
Suba-1985 <134758157+Suba-1985@users.noreply.github.com>
2024-04-30 21:52:27 -0400

    Update TestContext.java
    updated POM manager

--- src/test/java/context/TestContext.java
+++ src/test/java/context/TestContext.java
@@ -10,6 +10,15 @@ public class TestContext {
 	private DriverManager webDriverManager;
 	private PageObjectManager pageObjectManager;
 	private CommonUtils commonUtils;
+	private BatchPage batchPage;
+	private WebDriver driver;
+	private LoginPage loginPage;
+	private HomePage homePage;
+	private DashboardPage dashboardPage;
+	private UserPage_VerifySort userVerifySort;
+	private UserPage_Edit userPage_Edit;
+	private ManageProgramPage mp;
+	
 
 	public TestContext() {
 		webDriverManager = new DriverManager();
@@ -28,5 +37,10 @@ public class TestContext {
 	public CommonUtils getCommonUtils() {
 
 		return commonUtils;
+	}
+
+	public BatchPage getBatchPage()
+	{ 
+		return (batchPage == null) ? batchPage = new BatchPage(driver) : batchPage; 
 	}
 }
