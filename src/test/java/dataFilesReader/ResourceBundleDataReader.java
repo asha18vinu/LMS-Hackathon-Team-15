9b6296aff6bdf5d9946502e499320be355d39707
suba <suba>
2024-05-02 02:23:01 -0400

    c

--- src/test/java/dataFilesReader/ResourceBundleDataReader.java
+++ src/test/java/dataFilesReader/ResourceBundleDataReader.java
@@ -10,7 +10,7 @@ public class ResourceBundleDataReader {
 	ResourceBundle dataRead = ResourceBundle.getBundle("configTestData/configTestData");
 
 	public BrowserType getBrowserType() {
-<<<<<<< HEAD
+
 		
 	//	***************These are the code used to Run from different browser**************//
 		String browserName = TestRunner.BROWSER.get();		//******Browser value from testng.xml to testrunner to here. uncomment this if you want to execute from testngcb.xml
@@ -20,19 +20,13 @@ public class ResourceBundleDataReader {
 		
 		
 	//****************************************************************************************//
-		//String browserName = dataRead.getString("browser");
-		//System.out.println(browserName);
-=======
-		// ***************These are the code used to Run from different
-		// browser**************//
-		String browserName = TestRunner.BROWSER.get(); // ******Browser value from testng.xml to testrunner to here.
-														// uncomment this if you want to execute from testngcb.xml
+		
 
-		// ****************************************************************************************//
+		
 		// String browserName = dataRead.getString("browser");
 		// System.out.println(browserName);
 		System.out.println(browserName);
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
 		if (browserName == null || browserName.equalsIgnoreCase("chrome"))
 			return BrowserType.CHROME;
 
@@ -53,7 +47,7 @@ public class ResourceBundleDataReader {
 		else
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
-<<<<<<< HEAD
+
 	
 	public String getManageBatchHeader() 
 	{
@@ -64,20 +58,17 @@ public class ResourceBundleDataReader {
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
 	
-	public String getUrl() 
-	{
-=======
+	
 
 	public String getUrl() {
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
 		String url = dataRead.getString("url");
 		if (url != null)
 			return url;
 		else
 			throw new RuntimeException("Url not specified in the file.");
 	}
-<<<<<<< HEAD
-	
+
 	public String getBatchUrl() 
 	{
 		String batchpageUrl = dataRead.getString("batchpageUrl");
@@ -96,9 +87,9 @@ public class ResourceBundleDataReader {
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
 	
-=======
 
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
+
 	public String getScreenshotPath() {
 		String screenshotPath = dataRead.getString("screenshotPath");
 		if (screenshotPath != null)
@@ -106,77 +97,51 @@ public class ResourceBundleDataReader {
 		else
 			throw new RuntimeException("ScreenshotPath not specified in the file.");
 	}
-<<<<<<< HEAD
+
 	
-	public String getExcelTestData() 
+	public String getExcelTestData1() 
 	{
 		String BatchModuleexcelTestData = dataRead.getString("BatchModuleexcelTestData");
 		if (BatchModuleexcelTestData != null)
 			return BatchModuleexcelTestData;
-=======
+	}
 
 	public String getExcelTestData() {
 		String excelTestData = dataRead.getString("excelTestData");
 		if (excelTestData != null)
 			return excelTestData;
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
 		else
 			throw new RuntimeException("implicit Wait not specified in the file.");
-	}
-
-<<<<<<< HEAD
 	
-	public String getUserName() 
-	{
-=======
-	public String getBatchUrl() {
-		String batchpageUrl = dataRead.getString("batchpageUrl");
-		if (batchpageUrl != null)
-			return batchpageUrl;
-		else
-			throw new RuntimeException("implicit Wait not specified in the file.");
-	}
 
-	public String getBatchPopboxHeader() {
-		String batchPopUpBoxHeader = dataRead.getString("batchPopUpBoxHeader");
-		if (batchPopUpBoxHeader != null)
-			return batchPopUpBoxHeader;
-		else
-			throw new RuntimeException("implicit Wait not specified in the file.");
-	}
 
-	public String getManageBatchHeader() {
-		String BatchHeader = dataRead.getString("BatchHeader");
-		if (BatchHeader != null)
-			return BatchHeader;
-		else
-			throw new RuntimeException("implicit Wait not specified in the file.");
-	}
 
-	public String getUserName() {
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+	
+
+	
+
+	public String getUserName()	{
+
 		String userName = dataRead.getString("userName");
 		if (userName != null)
 			return userName;
 		else
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
-<<<<<<< HEAD
-	public String getPassword() 
-	{
-=======
+
+	
+
 
 	public String getPassword() {
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
 		String password = dataRead.getString("password");
 		if (password != null)
 			return password;
 		else
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
-<<<<<<< HEAD
-	
-=======
+
 
 	public String getUserUrl() {
 		String url = dataRead.getString("userUrl");
@@ -194,5 +159,5 @@ public class ResourceBundleDataReader {
 			throw new RuntimeException("implicit Wait not specified in the file.");
 	}
 
->>>>>>> 827f3cef0c4a9a6baa5e6cfbc2f265fbce3f2a45
+
 }
